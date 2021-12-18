2283916daf24ea217bfb2402834fe00752c69856
李奕鹏 <45395042+liyipeng123@users.noreply.github.com>
2021-12-18 18:43:10 +0800

    feature: add webpack for development

--- src/curserAgent.ts
+++ src/curserAgent.ts
@@ -1,34 +1,35 @@
 import BlurCurser from "./blurCurser";
 
 class curserAgent{
-    curser;
+    curser:any;
+    private createSingleCurser: () => any;
     constructor(){
         this.createSingleCurser = this.getSingleton(this.createBlurCurser);
     }
 
      createBlurCurser(){
-        const curserDom = document.createElement("div") 
+        const curserDom = window.document.createElement("div")
         curserDom.className = "curser-dom"
-        const curserWrapper = document.createElement("div") 
+        const curserWrapper = document.createElement("div")
         curserWrapper.className = "curser-wrapper"
         curserWrapper.appendChild(curserDom)
         return curserWrapper;
     }
-    
-     getSingleton(fn){
-        let result;
+
+     getSingleton(fn: { (): HTMLDivElement; apply?: any; }){
+        let result: any;
         return function(){
            return result || (result = fn.apply(this,arguments));
         }
     }
 
-    update(e){
+    update(e: any){
             console.log(e)
             this.curser.clientX = e.clientX;
             this.curser.clientY = e.clientY;
     }
-    
-    
+
+
     init(){
         this.curser = this.createSingleCurser();
         document.body.appendChild(this.curser);
